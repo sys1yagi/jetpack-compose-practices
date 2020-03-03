0db609dd86a77e56bc888b6f9bb3e75f0867c740
Toshihiro Yagi <sylc.yagi@gmail.com>
2020-03-04 08:34:55 +0900

    Add main page

--- app/src/main/java/com/sys1yagi/jetpack_compose_practices/MainActivity.kt
+++ app/src/main/java/com/sys1yagi/jetpack_compose_practices/MainActivity.kt
@@ -5,29 +5,62 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
 import androidx.ui.core.Text
 import androidx.ui.core.setContent
+import androidx.ui.foundation.Clickable
+import androidx.ui.foundation.VerticalScroller
+import androidx.ui.foundation.shape.corner.RoundedCornerShape
+import androidx.ui.layout.Column
+import androidx.ui.layout.LayoutHeight
+import androidx.ui.layout.LayoutPadding
+import androidx.ui.layout.LayoutWidth
+import androidx.ui.material.AppBarIcon
 import androidx.ui.material.MaterialTheme
+import androidx.ui.material.TopAppBar
+import androidx.ui.material.ripple.Ripple
+import androidx.ui.material.surface.Card
+import androidx.ui.res.stringResource
 import androidx.ui.tooling.preview.Preview
+import androidx.ui.unit.dp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MaterialTheme {
-                Greeting("Android")
-            }
+            MainPage()
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
 @Preview
 @Composable
-fun DefaultPreview() {
+fun MainPage() {
     MaterialTheme {
-        Greeting("Android")
+        Column(
+                modifier = LayoutHeight.Fill
+        ) {
+            TopAppBar(
+                    title = { Text(stringResource(id = R.string.app_name)) },
+                    elevation = 2.dp
+            )
+            PracticeList()
+        }
+    }
+}
+fun PracticeList() {
+    VerticalScroller {
+        Column {
+            Ripple(bounded = true) {
+                Clickable(onClick = {}) {
+                    Card(
+                            modifier = LayoutPadding(16.dp) + LayoutWidth.Fill,
+                            shape = RoundedCornerShape(4.dp),
+                            elevation = 4.dp
+                    ) {
+                        Text("Clickable Card",
+                                modifier = LayoutPadding(16.dp)
+                        )
+                    }
+                }
+            }
+        }
     }
 }
